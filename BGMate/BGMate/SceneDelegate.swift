205b6e25265905fc8bfa1b5ade57ecf1459f51c7
권태우 TaewooKéplerKwon <kwontaewoo0901@gmail.com>
2025-07-30 14:11:46 +0900

    Update SceneDelegate.swift

--- BGMate/BGMate/SceneDelegate.swift
+++ BGMate/BGMate/SceneDelegate.swift
@@ -18,13 +18,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let window = UIWindow(windowScene: windowScene)
         
         let tabOneNav = UINavigationController(rootViewController: HomeViewController())  // 홈(임시 파일 생성
-        tabOneNav.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 0)
+        tabOneNav.tabBarItem = UITabBarItem(title: "BGMate", image: UIImage(systemName: "house"), tag: 0)
 //
 //        let tabTwoNav = UINavigationController(rootViewController: TabTwoViewController())  // 전체 리스트
 //        tabTwoNav.tabBarItem = UITabBarItem(title: "Tab02", image: UIImage(systemName: "map.fill"), tag: 1)
       
         let tabThreeNav = UINavigationController(rootViewController: SearchViewController())  // 검색
-        tabThreeNav.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "headphones.circle"), tag: 2)
+        tabThreeNav.tabBarItem = UITabBarItem(title: "SEARCH", image: UIImage(systemName: "magnifyingglass"), tag: 2)
 //
 //        let tabFourNav = UINavigationController(rootViewController: CreatePlaylistViewController())  //프로필
 //        tabFourNav.tabBarItem = UITabBarItem(title: "Tab04", image: UIImage(systemName: "map"), tag: 3)
