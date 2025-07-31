ccf837b00d062036c6540d2ff3be93ad6031c1c5
권태우 TaewooKéplerKwon <kwontaewoo0901@gmail.com>
2025-07-31 15:47:48 +0900

    Create Song.swift

---
+++ BGMate/BGMate/Models/Song.swift
@@ -0,0 +1,22 @@
+//
+//  Song.swift
+//  BGMate
+//
+//  Created by catharina J on 7/29/25.
+//
+
+import Foundation
+
+struct Song: Identifiable {
+    let id: UUID
+    let title: String
+    let artist: String
+    let tags: [String]
+    let fileName: String
+}
+
+let songs: [Song] = [
+    Song(id: UUID(), title: "Sunset Lounge", artist: "LofiBeats", tags: ["cafe", "relax", "evening"], fileName:"burbank - sorry i like you"),
+    Song(id: UUID(), title: "Fresh Morning", artist: "AcousticFlow", tags: ["brunch", "bright", "morning"], fileName: "Chill Noons"),
+    Song(id: UUID(), title: "Minimal Vibe", artist: "ChillSpace", tags: ["modern", "minimal", "gallery"], fileName: "watermelt - summer")
+]
