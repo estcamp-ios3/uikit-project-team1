2328d83aa152a24c55221385f2d4972549299719
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2025-08-04 17:47:16 +0900

    Update SearchViewController.swift

--- BGMate/BGMate/Home/SearchViewController.swift
+++ BGMate/BGMate/Home/SearchViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 
+// View controller responsible for the Search screen
 class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
+    // Declaration of search bar and table view
     let searchBar = UISearchBar()
     let tableView = UITableView()
     
+    // Full tag list and filtered results
     let results = tagList
     var filteredResults: [Tags] = []
     
@@ -20,14 +23,17 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         self.view.backgroundColor = .systemBackground
         self.title = "SEARCH"
         
+        // UI setup
         setupSearchBar()
         setupTableView()
         
+        // Add tap gesture to dismiss the keyboard
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         tapGesture.cancelsTouchesInView = false
         view.addGestureRecognizer(tapGesture)
     }
     
+    // Configure the search bar
     private func setupSearchBar() {
         searchBar.placeholder = "Type to search..."
         searchBar.delegate = self
@@ -35,6 +41,7 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(searchBar)
         
+        // Set up auto layout constraints
         NSLayoutConstraint.activate([
             searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -42,6 +49,7 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         ])
     }
     
+    // Configure the table view
     private func setupTableView() {
         tableView.delegate = self
         tableView.dataSource = self
@@ -49,6 +57,7 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
         
+        // Set up auto layout constraints
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -59,10 +68,12 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     // MARK: - TableView DataSource
     
+    // Return number of cells in section
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return filteredResults.count
     }
     
+    // Configure cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
         
@@ -71,6 +82,7 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         return cell
     }
     
+    // Navigate to music player screen when a cell is selected
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         playAllMusic()
@@ -78,6 +90,7 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     // MARK: - SearchBar Delegate
     
+    // Perform filtering when search text changes
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchText.isEmpty {
             filteredResults.removeAll()
@@ -87,6 +100,7 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         tableView.reloadData()
     }
     
+    // Reset state when cancel button is clicked
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searchBar.text = ""
         filteredResults.removeAll()
@@ -94,10 +108,12 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         searchBar.resignFirstResponder()
     }
     
+    // Method to dismiss the keyboard
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
     
+    // Method to present the music player screen modally
     @objc func playAllMusic() {
         let playerVC = PlayerViewController()
         playerVC.musicList = Playlist(title: "Ronaldo, the GOAT", coverImageName: "calm_cover", playlist: [songs[0], songs[1], songs[2]])
