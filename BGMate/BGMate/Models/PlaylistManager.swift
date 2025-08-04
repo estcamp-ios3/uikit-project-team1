0c238f5ac64f1406a8516f7d49329f6fc8bb39bc
akarisoho <akarisoho1@gmail.com>
2025-08-04 21:07:08 +0900

    core: PlaylistManager + PlaylistStorage 싱글톤 병합

--- BGMate/BGMate/Models/PlaylistManager.swift
+++ BGMate/BGMate/Models/PlaylistManager.swift
@@ -6,9 +6,8 @@
 //
 
 import Foundation
-import Combine
 
-class PlaylistManager: ObservableObject {
+class PlaylistManager {
     static let shared = PlaylistManager()
     
     private init() {
@@ -16,7 +15,7 @@ class PlaylistManager: ObservableObject {
     }
     
     /// 앱 전역에서 공유되는 플레이리스트
-    @Published var playlists: [Playlist] = [] {
+    var playlists: [Playlist] = [] {
         didSet {
             savePlaylists()
         }
@@ -47,4 +46,39 @@ class PlaylistManager: ObservableObject {
          playlists.append(playlist)
          NotificationCenter.default.post(name: .playlistCreated, object: nil)
      }
+    
+    // MusicPlayerVC에 새로운 곡 추가
+    func save(_ playlist: Playlist) {
+        if let data = try? JSONEncoder().encode(playlist) {
+            UserDefaults.standard.set(data, forKey: key)
+        }
+    }
+    
+    // MARK: - 곡 추가후 playlist 업데이트 할 때 사용하는거면 함수이름 update()로 변경해서 사용
+//
+//    func load() -> Playlist? {
+//        if let data = UserDefaults.standard.data(forKey: key),
+//           let playlist = try? JSONDecoder().decode(Playlist.self, from: data) {
+//            return playlist
+//        }
+//        return nil
+//    }
+    
+    
+    // MARK: - PlaylistManager 설계 당시 있었던 곡 추가/삭제 함수 (MusicPlayerVC에 미사용하면 지워주세요)
+    
+//    /// 곡 추가
+//    func addSong(_ song: Song, to playlistID: UUID) {
+//        if let index = playlists.firstIndex(where: { $0.id == playlistID }) {
+//            playlists[index].playlist.append(song)
+//        }
+//    }
+//
+//    /// 곡 삭제
+//    func removeSong(songID: UUID, from playlistID: UUID) {
+//        if let index = playlists.firstIndex(where: { $0.id == playlistID }) {
+//            playlists[index].playlist.removeAll { $0.id == songID }
+//        }
+//    }
+
 }
