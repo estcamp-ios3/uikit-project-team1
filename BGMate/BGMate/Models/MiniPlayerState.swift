6754ae61999d48fb43c89b4fe2dcc561c07ac3b4
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2025-08-05 14:11:44 +0900

    Create MiniPlayerState.swift

---
+++ BGMate/BGMate/Models/MiniPlayerState.swift
@@ -0,0 +1,13 @@
+//
+//  MiniPlayerState.swift
+//  BGMate
+//
+//  Created by 권태우 on 8/5/25.
+//
+
+class MiniPlayerState {
+    static let shared = MiniPlayerState()
+    private init() {}
+
+    var isMiniPlayerVisible: Bool = false
+}
